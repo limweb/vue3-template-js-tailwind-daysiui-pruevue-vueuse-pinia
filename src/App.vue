491fbf03399590e6fdf9c33e498b5f6d8ccab2c1
limweb@hotmail.com <limweb@hotmail.com>
2024-02-29 00:37:56 +0700

    add headlessui

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <main class="flex flex-col">
     js/tailwindcss/pinia/vue3/vueUse/Prumvue/daisyui
+    <Helloworld />
   </main>
 </template>
 <script setup>
 import { useMouse } from '@vueuse/core'
+import Helloworld from './components/Helloworld.vue';
 const mouse = useMouse()
 console.log(mouse.x.value)
 </script>
